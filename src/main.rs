4abea477e91ae2f47eebf047976002d58d2ae00e
Guocork <g1024536444@gmail.com>
2024-03-10 11:40:47 +0800

    引入相关依赖 完善项目结构

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::{get, post},
+    http::StatusCode,
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
+
+#[tokio::main]
+async fn main() {
+    
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/users", post(create_user));
+
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+
+async fn create_user(
+    Json(payload): Json<CreateUser>,
+) -> (StatusCode, Json<User>) {
+
+    let user = User {
+        id: 1337,
+        username: payload.username,
+    };
+
+    (StatusCode::CREATED, Json(user))
+}
+
+
+#[derive(Deserialize)]
+struct CreateUser {
+    username: String,
+}
+
+
+#[derive(Serialize)]
+struct User {
+    id: u64,
+    username: String,
 }
